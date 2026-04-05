727a23a2a7c681d914b6168ee69238afa2132de9
CPT-Ibrahim <amroibraheem717@gmail.com>
2026-04-05 20:56:30 +0100

    Dependancies is FIXED - now you can run the app normally

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.atmbanksimulator {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires mysql.connector.j;
 
     opens com.atmbanksimulator to javafx.fxml;
     exports com.atmbanksimulator;
